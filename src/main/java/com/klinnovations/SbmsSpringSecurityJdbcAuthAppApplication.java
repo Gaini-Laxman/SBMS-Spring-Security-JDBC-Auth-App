7d4a675c46192a3a6610e5c61d2c0c02b429c745
Gaini Laxman <gainilaxman12@gmail.com>
2023-05-30 22:49:14 +0530

    initial commit

---
+++ src/main/java/com/klinnovations/SbmsSpringSecurityJdbcAuthAppApplication.java
@@ -0,0 +1,13 @@
+package com.klinnovations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbmsSpringSecurityJdbcAuthAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbmsSpringSecurityJdbcAuthAppApplication.class, args);
+	}
+
+}
